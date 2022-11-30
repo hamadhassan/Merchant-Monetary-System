c51ef4836a910c4082afebd27e49f98135f25b31
Muhammad Hammad Hassan <mhhjutt@gmail.com>
2022-11-30 19:50:48 +0500

    Dev-U06-UI

---
+++ Application/Merchant Monetary System/frmViewAndDeleteAccount.Designer.cs
@@ -0,0 +1,288 @@
+﻿namespace Merchant_Monetary_System
+{
+    partial class frmViewAndDeleteAccount
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.gbx = new System.Windows.Forms.GroupBox();
+            this.btnGo = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtbxSearch = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cmbxDesignation = new System.Windows.Forms.ComboBox();
+            this.cmbxFiliter = new System.Windows.Forms.ComboBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.txtbxAttributes = new System.Windows.Forms.ComboBox();
+            this.dataGridView = new System.Windows.Forms.DataGridView();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnEdit = new System.Windows.Forms.Button();
+            this.gbx.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // gbx
+            // 
+            this.gbx.Controls.Add(this.dataGridView);
+            this.gbx.Controls.Add(this.txtbxSearch);
+            this.gbx.Controls.Add(this.label3);
+            this.gbx.Controls.Add(this.txtbxAttributes);
+            this.gbx.Controls.Add(this.cmbxFiliter);
+            this.gbx.Controls.Add(this.label1);
+            this.gbx.Controls.Add(this.btnGo);
+            this.gbx.Controls.Add(this.btnClear);
+            this.gbx.Controls.Add(this.btnEdit);
+            this.gbx.Controls.Add(this.btnDelete);
+            this.gbx.Controls.Add(this.btnClose);
+            this.gbx.Controls.Add(this.cmbxDesignation);
+            this.gbx.Controls.Add(this.label2);
+            this.gbx.Controls.Add(this.label6);
+            this.gbx.Controls.Add(this.label10);
+            this.gbx.Location = new System.Drawing.Point(12, 12);
+            this.gbx.Name = "gbx";
+            this.gbx.Size = new System.Drawing.Size(906, 516);
+            this.gbx.TabIndex = 0;
+            this.gbx.TabStop = false;
+            // 
+            // btnGo
+            // 
+            this.btnGo.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(25)))), ((int)(((byte)(100)))), ((int)(((byte)(26)))));
+            this.btnGo.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.btnGo.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnGo.ForeColor = System.Drawing.Color.White;
+            this.btnGo.Location = new System.Drawing.Point(802, 87);
+            this.btnGo.Name = "btnGo";
+            this.btnGo.Size = new System.Drawing.Size(90, 30);
+            this.btnGo.TabIndex = 14;
+            this.btnGo.Text = "Go";
+            this.btnGo.UseVisualStyleBackColor = false;
+            // 
+            // btnClear
+            // 
+            this.btnClear.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(14)))), ((int)(((byte)(97)))), ((int)(((byte)(139)))));
+            this.btnClear.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.btnClear.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnClear.ForeColor = System.Drawing.Color.White;
+            this.btnClear.Location = new System.Drawing.Point(706, 87);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(90, 30);
+            this.btnClear.TabIndex = 16;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = false;
+            // 
+            // btnClose
+            // 
+            this.btnClose.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(200)))), ((int)(((byte)(37)))), ((int)(((byte)(48)))));
+            this.btnClose.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.btnClose.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnClose.ForeColor = System.Drawing.Color.White;
+            this.btnClose.Location = new System.Drawing.Point(610, 480);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(90, 30);
+            this.btnClose.TabIndex = 15;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(328, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(251, 39);
+            this.label1.TabIndex = 17;
+            this.label1.Text = "Account Detail";
+            // 
+            // txtbxSearch
+            // 
+            this.txtbxSearch.Location = new System.Drawing.Point(552, 55);
+            this.txtbxSearch.Name = "txtbxSearch";
+            this.txtbxSearch.Size = new System.Drawing.Size(142, 26);
+            this.txtbxSearch.TabIndex = 20;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(486, 58);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(68, 20);
+            this.label3.TabIndex = 28;
+            this.label3.Text = "Search :";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(260, 59);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(86, 20);
+            this.label6.TabIndex = 29;
+            this.label6.Text = "Attributes :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(14, 57);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(102, 20);
+            this.label2.TabIndex = 30;
+            this.label2.Text = "Designation :";
+            // 
+            // cmbxDesignation
+            // 
+            this.cmbxDesignation.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.cmbxDesignation.FormattingEnabled = true;
+            this.cmbxDesignation.Items.AddRange(new object[] {
+            "CEO",
+            "Employee",
+            "Rider",
+            "Warehouse Manager"});
+            this.cmbxDesignation.Location = new System.Drawing.Point(114, 53);
+            this.cmbxDesignation.Name = "cmbxDesignation";
+            this.cmbxDesignation.Size = new System.Drawing.Size(142, 28);
+            this.cmbxDesignation.Sorted = true;
+            this.cmbxDesignation.TabIndex = 19;
+            // 
+            // cmbxFiliter
+            // 
+            this.cmbxFiliter.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.cmbxFiliter.FormattingEnabled = true;
+            this.cmbxFiliter.Items.AddRange(new object[] {
+            "Ascending Order",
+            "Contain",
+            "Descending Order",
+            "End With ",
+            "Start With"});
+            this.cmbxFiliter.Location = new System.Drawing.Point(750, 53);
+            this.cmbxFiliter.Name = "cmbxFiliter";
+            this.cmbxFiliter.Size = new System.Drawing.Size(142, 28);
+            this.cmbxFiliter.Sorted = true;
+            this.cmbxFiliter.TabIndex = 19;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(698, 58);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(55, 20);
+            this.label10.TabIndex = 30;
+            this.label10.Text = "Filiter :";
+            // 
+            // txtbxAttributes
+            // 
+            this.txtbxAttributes.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.txtbxAttributes.FormattingEnabled = true;
+            this.txtbxAttributes.Items.AddRange(new object[] {
+            "CNIC Number",
+            "Contact Number",
+            "Email Address",
+            "Gender",
+            "Home Address",
+            "Name",
+            "Username"});
+            this.txtbxAttributes.Location = new System.Drawing.Point(343, 53);
+            this.txtbxAttributes.Name = "txtbxAttributes";
+            this.txtbxAttributes.Size = new System.Drawing.Size(142, 28);
+            this.txtbxAttributes.Sorted = true;
+            this.txtbxAttributes.TabIndex = 19;
+            // 
+            // dataGridView
+            // 
+            this.dataGridView.BackgroundColor = System.Drawing.SystemColors.Control;
+            this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView.Location = new System.Drawing.Point(18, 123);
+            this.dataGridView.Name = "dataGridView";
+            this.dataGridView.Size = new System.Drawing.Size(874, 351);
+            this.dataGridView.TabIndex = 31;
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(218)))), ((int)(((byte)(216)))), ((int)(((byte)(100)))));
+            this.btnDelete.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.btnDelete.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnDelete.ForeColor = System.Drawing.Color.Black;
+            this.btnDelete.Location = new System.Drawing.Point(706, 480);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(90, 30);
+            this.btnDelete.TabIndex = 15;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = false;
+            // 
+            // btnEdit
+            // 
+            this.btnEdit.BackColor = System.Drawing.Color.Black;
+            this.btnEdit.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.btnEdit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnEdit.ForeColor = System.Drawing.Color.White;
+            this.btnEdit.Location = new System.Drawing.Point(802, 480);
+            this.btnEdit.Name = "btnEdit";
+            this.btnEdit.Size = new System.Drawing.Size(90, 30);
+            this.btnEdit.TabIndex = 15;
+            this.btnEdit.Text = "Edit";
+            this.btnEdit.UseVisualStyleBackColor = false;
+            // 
+            // frmViewAndDeleteAccount
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(930, 540);
+            this.Controls.Add(this.gbx);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "frmViewAndDeleteAccount";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Account Detail";
+            this.gbx.ResumeLayout(false);
+            this.gbx.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox gbx;
+        private System.Windows.Forms.Button btnGo;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtbxSearch;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox cmbxDesignation;
+        private System.Windows.Forms.DataGridView dataGridView;
+        private System.Windows.Forms.ComboBox txtbxAttributes;
+        private System.Windows.Forms.ComboBox cmbxFiliter;
+        private System.Windows.Forms.Button btnEdit;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Label label10;
+    }
+}
