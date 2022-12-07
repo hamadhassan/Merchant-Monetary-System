87f99deb489f2ac3804f54b225b829d2b326c39f
KabirAhmad4 <ulhaq8990@gmail.com>
2022-12-07 23:13:41 +0500

    Dev-U08-UI

---
+++ Application/Merchant Monetary System/Forms/Product/frmAddProduct.Designer.cs
@@ -0,0 +1,357 @@
+﻿namespace Merchant_Monetary_System.Forms.Product
+{
+    partial class frmAddProduct
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblVehicle = new System.Windows.Forms.Label();
+            this.cmbxSenstivity = new System.Windows.Forms.ComboBox();
+            this.lblNewPasswordSignal = new System.Windows.Forms.Label();
+            this.lblUsernameSignal = new System.Windows.Forms.Label();
+            this.lblNameSignal = new System.Windows.Forms.Label();
+            this.lblDesignationSingal = new System.Windows.Forms.Label();
+            this.btnAddProduct = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.txtbxVolume = new System.Windows.Forms.TextBox();
+            this.txtbxWeight = new System.Windows.Forms.TextBox();
+            this.txtbxSKU_ID = new System.Windows.Forms.TextBox();
+            this.txtbxName = new System.Windows.Forms.TextBox();
+            this.lblNewPassword = new System.Windows.Forms.Label();
+            this.lblUsername = new System.Windows.Forms.Label();
+            this.lblConfirmPassword = new System.Windows.Forms.Label();
+            this.lblName = new System.Windows.Forms.Label();
+            this.lblDesignation = new System.Windows.Forms.Label();
+            this.cmbxManufacture = new System.Windows.Forms.ComboBox();
+            this.lblSignUp = new System.Windows.Forms.Label();
+            this.gbx = new System.Windows.Forms.GroupBox();
+            this.comboCategory = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.gbx.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lblVehicle
+            // 
+            this.lblVehicle.AutoSize = true;
+            this.lblVehicle.Location = new System.Drawing.Point(486, 118);
+            this.lblVehicle.Name = "lblVehicle";
+            this.lblVehicle.Size = new System.Drawing.Size(73, 13);
+            this.lblVehicle.TabIndex = 27;
+            this.lblVehicle.Text = "Manufacture :";
+            this.lblVehicle.Click += new System.EventHandler(this.lblVehicle_Click);
+            // 
+            // cmbxSenstivity
+            // 
+            this.cmbxSenstivity.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbxSenstivity.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.cmbxSenstivity.FormattingEnabled = true;
+            this.cmbxSenstivity.Items.AddRange(new object[] {
+            "CEO",
+            "Employee",
+            "Rider"});
+            this.cmbxSenstivity.Location = new System.Drawing.Point(642, 167);
+            this.cmbxSenstivity.Name = "cmbxSenstivity";
+            this.cmbxSenstivity.Size = new System.Drawing.Size(203, 21);
+            this.cmbxSenstivity.Sorted = true;
+            this.cmbxSenstivity.TabIndex = 14;
+            // 
+            // lblNewPasswordSignal
+            // 
+            this.lblNewPasswordSignal.AutoSize = true;
+            this.lblNewPasswordSignal.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(200)))), ((int)(((byte)(37)))), ((int)(((byte)(48)))));
+            this.lblNewPasswordSignal.Location = new System.Drawing.Point(227, 243);
+            this.lblNewPasswordSignal.Name = "lblNewPasswordSignal";
+            this.lblNewPasswordSignal.Size = new System.Drawing.Size(10, 13);
+            this.lblNewPasswordSignal.TabIndex = 21;
+            this.lblNewPasswordSignal.Text = " ";
+            // 
+            // lblUsernameSignal
+            // 
+            this.lblUsernameSignal.AutoSize = true;
+            this.lblUsernameSignal.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(200)))), ((int)(((byte)(37)))), ((int)(((byte)(48)))));
+            this.lblUsernameSignal.Location = new System.Drawing.Point(227, 193);
+            this.lblUsernameSignal.Name = "lblUsernameSignal";
+            this.lblUsernameSignal.Size = new System.Drawing.Size(10, 13);
+            this.lblUsernameSignal.TabIndex = 20;
+            this.lblUsernameSignal.Text = " ";
+            // 
+            // lblNameSignal
+            // 
+            this.lblNameSignal.AutoSize = true;
+            this.lblNameSignal.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(200)))), ((int)(((byte)(37)))), ((int)(((byte)(48)))));
+            this.lblNameSignal.Location = new System.Drawing.Point(227, 141);
+            this.lblNameSignal.Name = "lblNameSignal";
+            this.lblNameSignal.Size = new System.Drawing.Size(10, 13);
+            this.lblNameSignal.TabIndex = 19;
+            this.lblNameSignal.Text = " ";
+            // 
+            // lblDesignationSingal
+            // 
+            this.lblDesignationSingal.AutoSize = true;
+            this.lblDesignationSingal.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(200)))), ((int)(((byte)(37)))), ((int)(((byte)(48)))));
+            this.lblDesignationSingal.Location = new System.Drawing.Point(639, 143);
+            this.lblDesignationSingal.Name = "lblDesignationSingal";
+            this.lblDesignationSingal.Size = new System.Drawing.Size(10, 13);
+            this.lblDesignationSingal.TabIndex = 1;
+            this.lblDesignationSingal.Text = " ";
+            // 
+            // btnAddProduct
+            // 
+            this.btnAddProduct.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(25)))), ((int)(((byte)(100)))), ((int)(((byte)(26)))));
+            this.btnAddProduct.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.btnAddProduct.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnAddProduct.ForeColor = System.Drawing.Color.White;
+            this.btnAddProduct.Location = new System.Drawing.Point(566, 406);
+            this.btnAddProduct.Name = "btnAddProduct";
+            this.btnAddProduct.Size = new System.Drawing.Size(200, 30);
+            this.btnAddProduct.TabIndex = 13;
+            this.btnAddProduct.Text = "Add Product";
+            this.btnAddProduct.UseVisualStyleBackColor = false;
+            // 
+            // btnClear
+            // 
+            this.btnClear.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(14)))), ((int)(((byte)(97)))), ((int)(((byte)(139)))));
+            this.btnClear.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.btnClear.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnClear.ForeColor = System.Drawing.Color.White;
+            this.btnClear.Location = new System.Drawing.Point(348, 406);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(200, 30);
+            this.btnClear.TabIndex = 12;
+            this.btnClear.Text = "Clear All";
+            this.btnClear.UseVisualStyleBackColor = false;
+            // 
+            // btnClose
+            // 
+            this.btnClose.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(200)))), ((int)(((byte)(37)))), ((int)(((byte)(48)))));
+            this.btnClose.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.btnClose.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnClose.ForeColor = System.Drawing.Color.White;
+            this.btnClose.Location = new System.Drawing.Point(142, 406);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(200, 30);
+            this.btnClose.TabIndex = 11;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = false;
+            // 
+            // txtbxVolume
+            // 
+            this.txtbxVolume.Location = new System.Drawing.Point(227, 263);
+            this.txtbxVolume.Name = "txtbxVolume";
+            this.txtbxVolume.Size = new System.Drawing.Size(149, 20);
+            this.txtbxVolume.TabIndex = 4;
+            this.txtbxVolume.UseSystemPasswordChar = true;
+            // 
+            // txtbxWeight
+            // 
+            this.txtbxWeight.Location = new System.Drawing.Point(227, 214);
+            this.txtbxWeight.Name = "txtbxWeight";
+            this.txtbxWeight.Size = new System.Drawing.Size(149, 20);
+            this.txtbxWeight.TabIndex = 3;
+            this.txtbxWeight.UseSystemPasswordChar = true;
+            // 
+            // txtbxSKU_ID
+            // 
+            this.txtbxSKU_ID.Location = new System.Drawing.Point(227, 164);
+            this.txtbxSKU_ID.Name = "txtbxSKU_ID";
+            this.txtbxSKU_ID.Size = new System.Drawing.Size(203, 20);
+            this.txtbxSKU_ID.TabIndex = 2;
+            // 
+            // txtbxName
+            // 
+            this.txtbxName.Location = new System.Drawing.Point(227, 112);
+            this.txtbxName.Name = "txtbxName";
+            this.txtbxName.Size = new System.Drawing.Size(203, 20);
+            this.txtbxName.TabIndex = 1;
+            // 
+            // lblNewPassword
+            // 
+            this.lblNewPassword.AutoSize = true;
+            this.lblNewPassword.Location = new System.Drawing.Point(77, 216);
+            this.lblNewPassword.Name = "lblNewPassword";
+            this.lblNewPassword.Size = new System.Drawing.Size(47, 13);
+            this.lblNewPassword.TabIndex = 2;
+            this.lblNewPassword.Text = "Weight :";
+            // 
+            // lblUsername
+            // 
+            this.lblUsername.AutoSize = true;
+            this.lblUsername.Location = new System.Drawing.Point(77, 167);
+            this.lblUsername.Name = "lblUsername";
+            this.lblUsername.Size = new System.Drawing.Size(69, 13);
+            this.lblUsername.TabIndex = 2;
+            this.lblUsername.Text = "SKU Number";
+            this.lblUsername.Click += new System.EventHandler(this.lblUsername_Click);
+            // 
+            // lblConfirmPassword
+            // 
+            this.lblConfirmPassword.AutoSize = true;
+            this.lblConfirmPassword.Location = new System.Drawing.Point(77, 265);
+            this.lblConfirmPassword.Name = "lblConfirmPassword";
+            this.lblConfirmPassword.Size = new System.Drawing.Size(45, 13);
+            this.lblConfirmPassword.TabIndex = 2;
+            this.lblConfirmPassword.Text = "Volume:";
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Location = new System.Drawing.Point(77, 118);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(41, 13);
+            this.lblName.TabIndex = 2;
+            this.lblName.Text = "Name :";
+            // 
+            // lblDesignation
+            // 
+            this.lblDesignation.AutoSize = true;
+            this.lblDesignation.Location = new System.Drawing.Point(486, 170);
+            this.lblDesignation.Name = "lblDesignation";
+            this.lblDesignation.Size = new System.Drawing.Size(84, 13);
+            this.lblDesignation.TabIndex = 2;
+            this.lblDesignation.Text = "Sensitivity Type:";
+            this.lblDesignation.Click += new System.EventHandler(this.lblDesignation_Click);
+            // 
+            // cmbxManufacture
+            // 
+            this.cmbxManufacture.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbxManufacture.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.cmbxManufacture.FormattingEnabled = true;
+            this.cmbxManufacture.Items.AddRange(new object[] {
+            "CEO",
+            "Employee",
+            "Rider"});
+            this.cmbxManufacture.Location = new System.Drawing.Point(639, 115);
+            this.cmbxManufacture.Name = "cmbxManufacture";
+            this.cmbxManufacture.Size = new System.Drawing.Size(203, 21);
+            this.cmbxManufacture.Sorted = true;
+            this.cmbxManufacture.TabIndex = 0;
+            // 
+            // lblSignUp
+            // 
+            this.lblSignUp.AutoSize = true;
+            this.lblSignUp.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSignUp.Location = new System.Drawing.Point(365, 31);
+            this.lblSignUp.Name = "lblSignUp";
+            this.lblSignUp.Size = new System.Drawing.Size(215, 39);
+            this.lblSignUp.TabIndex = 0;
+            this.lblSignUp.Text = "Add Product";
+            // 
+            // gbx
+            // 
+            this.gbx.Controls.Add(this.label1);
+            this.gbx.Controls.Add(this.comboCategory);
+            this.gbx.Controls.Add(this.lblVehicle);
+            this.gbx.Controls.Add(this.cmbxSenstivity);
+            this.gbx.Controls.Add(this.lblNewPasswordSignal);
+            this.gbx.Controls.Add(this.lblUsernameSignal);
+            this.gbx.Controls.Add(this.lblNameSignal);
+            this.gbx.Controls.Add(this.lblDesignationSingal);
+            this.gbx.Controls.Add(this.btnAddProduct);
+            this.gbx.Controls.Add(this.btnClear);
+            this.gbx.Controls.Add(this.btnClose);
+            this.gbx.Controls.Add(this.txtbxVolume);
+            this.gbx.Controls.Add(this.txtbxWeight);
+            this.gbx.Controls.Add(this.txtbxSKU_ID);
+            this.gbx.Controls.Add(this.txtbxName);
+            this.gbx.Controls.Add(this.lblNewPassword);
+            this.gbx.Controls.Add(this.lblUsername);
+            this.gbx.Controls.Add(this.lblConfirmPassword);
+            this.gbx.Controls.Add(this.lblName);
+            this.gbx.Controls.Add(this.lblDesignation);
+            this.gbx.Controls.Add(this.cmbxManufacture);
+            this.gbx.Controls.Add(this.lblSignUp);
+            this.gbx.Location = new System.Drawing.Point(-9, -22);
+            this.gbx.Name = "gbx";
+            this.gbx.Size = new System.Drawing.Size(906, 516);
+            this.gbx.TabIndex = 1;
+            this.gbx.TabStop = false;
+            // 
+            // comboCategory
+            // 
+            this.comboCategory.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboCategory.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.comboCategory.FormattingEnabled = true;
+            this.comboCategory.Items.AddRange(new object[] {
+            "CEO",
+            "Employee",
+            "Rider"});
+            this.comboCategory.Location = new System.Drawing.Point(639, 216);
+            this.comboCategory.Name = "comboCategory";
+            this.comboCategory.Size = new System.Drawing.Size(203, 21);
+            this.comboCategory.Sorted = true;
+            this.comboCategory.TabIndex = 29;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(486, 219);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 13);
+            this.label1.TabIndex = 30;
+            this.label1.Text = "Category :";
+            // 
+            // frmAddProduct
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(888, 473);
+            this.Controls.Add(this.gbx);
+            this.Name = "frmAddProduct";
+            this.Text = "frmAddProduct";
+            this.Load += new System.EventHandler(this.frmAddProduct_Load);
+            this.gbx.ResumeLayout(false);
+            this.gbx.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label lblVehicle;
+        private System.Windows.Forms.ComboBox cmbxSenstivity;
+        private System.Windows.Forms.Label lblNewPasswordSignal;
+        private System.Windows.Forms.Label lblUsernameSignal;
+        private System.Windows.Forms.Label lblNameSignal;
+        private System.Windows.Forms.Label lblDesignationSingal;
+        private System.Windows.Forms.Button btnAddProduct;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.TextBox txtbxVolume;
+        private System.Windows.Forms.TextBox txtbxWeight;
+        private System.Windows.Forms.TextBox txtbxSKU_ID;
+        private System.Windows.Forms.TextBox txtbxName;
+        private System.Windows.Forms.Label lblNewPassword;
+        private System.Windows.Forms.Label lblUsername;
+        private System.Windows.Forms.Label lblConfirmPassword;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Label lblDesignation;
+        private System.Windows.Forms.ComboBox cmbxManufacture;
+        private System.Windows.Forms.Label lblSignUp;
+        private System.Windows.Forms.GroupBox gbx;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboCategory;
+    }
+}
---
+++ Application/Merchant Monetary System/Forms/Product/frmAddProduct.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Merchant_Monetary_System.Forms.Product
+{
+    public partial class frmAddProduct : Form
+    {
+        public frmAddProduct()
+        {
+            InitializeComponent();
+        }
+
+        private void frmAddProduct_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblDesignation_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblUsername_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblVehicle_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
