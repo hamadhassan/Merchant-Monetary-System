fa1a4f30754371fbc1f0402a002c0588cc0eb1b7
Hammad Hassan <mhhjutt@gmail.com>
2022-12-13 11:55:54 +0500

    Dev-U05-Logic
    The linked list data structure class is added.

---
+++ Application/Merchant Monetary System/Data Structure/DoublyLinkedList/DoublyLinkedListNode.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Merchant_Monetary_System
+{
+    public class DoublyLinkedListNode<T>
+    { 
+        public DoublyLinkedListNode(T data) => Data = data;
+        public T Data { get; }
+        public DoublyLinkedListNode<T> Next { get; set; }
+        public DoublyLinkedListNode<T> Previous { get; set; }
+    }
+}
