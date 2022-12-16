8483e333b38c17f34941c0a30884b8ac891dbc00
KabirAhmad4 <ulhaq8990@gmail.com>
2022-12-16 23:10:21 +0500

    Dev-U10-completed
    use case no 10 completed update product

--- Application/Merchant Monetary System/Forms/Product/frmUpdateProduct.cs
+++ Application/Merchant Monetary System/Forms/Product/frmUpdateProduct.cs
@@ -9,6 +9,7 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using Merchant_Monetary_System.DL;
 using Merchant_Monetary_System.BL;
+using System.Xml.Linq;
 
 namespace Merchant_Monetary_System.Forms.Product
 {
@@ -21,8 +22,19 @@ namespace Merchant_Monetary_System.Forms.Product
             product = previous_product;
         }
 
+        bool isName = true;
+        bool isSKU_Number = true;
+        bool isWeight = true;
+        bool isVolume = true;
         private void frmUpdateProduct_Load(object sender, EventArgs e)
         {
+            txtbxName.Text = product.Name;
+            txtbxSKU_ID.Text = product.SKU_Number.ToString();
+            txtbxWeight.Text = product.Weight.ToString();
+            txtbxVolume.Text = product.Volume.ToString();
+            comboCategory.Text=product.Category.ToString();
+            cmbxManufacture.Text = product.Manufacturer.ToString();
+            cmbxSenstivity.Text = product.SensitivityType.ToString();
 
         }
 
@@ -66,25 +78,6 @@ namespace Merchant_Monetary_System.Forms.Product
 
         }
 
-        private void txtbxVolume_TextChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void txtbxWeight_TextChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void txtbxSKU_ID_TextChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void txtbxName_TextChanged(object sender, EventArgs e)
-        {
-
-        }
 
         private void lblWeight_Click(object sender, EventArgs e)
         {
@@ -118,7 +111,32 @@ namespace Merchant_Monetary_System.Forms.Product
 
         private void btnAddProduct_Click(object sender, EventArgs e)
         {
-            
+            try
+            {
+                if (!isName && !isSKU_Number && !isWeight && !isVolume && comboCategory.Text != "" && cmbxManufacture.Text != "" && cmbxSenstivity.Text != "")
+                {
+                    string name = txtbxName.Text;
+                    int sku_number = Convert.ToInt16(txtbxSKU_ID.Text);
+                    double weight = Convert.ToInt64(txtbxWeight.Text);
+                    double Volume = Convert.ToInt64(txtbxVolume.Text);
+                    string manufacturer = cmbxManufacture.Text;
+                    string category = comboCategory.Text;
+                    string sensitivity = cmbxSenstivity.Text;
+                    BL.Product product = new BL. Product(name, sku_number, weight, Volume, manufacturer, sensitivity, category);
+                    ProductDL.updateRecord(product);
+                    BL.Product.storeRecordIntoFile(product, FilePath.Products);
+                    MessageBox.Show("Product successfully updated");
+                }
+                else
+                {
+                    MessageBox.Show("Product not updated");
+                }
+            }
+            catch (Exception exp)
+            {
+                MessageBox.Show(exp.Message);
+            }
+
         }
         private void clearField()
         {
@@ -138,6 +156,116 @@ namespace Merchant_Monetary_System.Forms.Product
         }
 
         private void lbl_Volume_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void txtbxName_TextChanged(object sender, EventArgs e)
+        {
+            int i;
+            if (txtbxName.Text == string.Empty)
+            {// check is empty
+                lblNameSignal.Text = "Enter the name";
+                isName = true;
+            }
+            else if (int.TryParse(txtbxName.Text, out i))
+            {//Check isnumberic
+                lblNameSignal.Text = "Allowed characters: a-z, A-Z";
+                isName = true;
+            }
+            else if (txtbxName.Text.Any(ch => !char.IsLetterOrDigit(ch)))
+            {//check isSpecialCharactor
+                lblNameSignal.Text = "Allowed characters: a-z, A-Z";
+                isName = true;
+            }
+            else
+            {//ready for storage or action
+                lblNameSignal.Text = " ";
+                isName = false;
+            }
+        }
+
+        private void gbx_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtbxSKU_ID_TextChanged(object sender, EventArgs e)
+        {
+            {
+                int i;
+                if (txtbxSKU_ID.Text == string.Empty)
+                {// check is empty
+                    lblS_Number.Text = "Enter the SKU number";
+                    isSKU_Number = true;
+                }
+                else if (!int.TryParse(txtbxSKU_ID.Text, out i))
+                {//Check isalphabetic
+                    lblS_Number.Text = "Allowed characters: 0-9";
+                    isSKU_Number = true;
+                }
+                else if (txtbxSKU_ID.Text.Any(ch => !char.IsLetterOrDigit(ch)))
+                {//check isSpecialCharactor
+                    lblS_Number.Text = "Allowed characters: 0-9";
+                    isSKU_Number = true;
+                }
+                else
+                {//ready for storage
+                    lblS_Number.Text = " ";
+                    isSKU_Number = false;
+                }
+            }
+        }
+
+        private void txtbxWeight_TextChanged(object sender, EventArgs e)
+        {
+            {
+                int i;
+                if (txtbxWeight.Text == string.Empty)
+                {// check is empty
+                    lbl_Weight.Text = "Enter the Weight";
+                    isWeight = true;
+                }
+                else if (txtbxWeight.Text.Any(ch => !char.IsDigit(ch)))
+                {//check isSpecialCharactor
+                    lbl_Weight.Text = "Allowed characters: 0-9";
+                    isWeight = true;
+                }
+                else
+                {//ready for storage
+                    lbl_Weight.Text = " ";
+                    isWeight = false;
+                }
+            }
+        }
+
+        private void txtbxVolume_TextChanged(object sender, EventArgs e)
+        {
+            {
+                int i;
+                if (txtbxVolume.Text == string.Empty)
+                {// check is empty
+                    lbl_Volume.Text = "Enter the Volume";
+                    isVolume = true;
+                }
+                //else if (!int.TryParse(txtbxSKU_ID.Text, out i))
+                //{//Check isalphabetic
+                //    lbl_Volume.Text = "Allowed characters: 0-9";
+                //    isVolume = true;
+                //}
+                else if (txtbxVolume.Text.Any(ch => !char.IsDigit(ch)))
+                {//check isSpecialCharactor
+                    lbl_Volume.Text = "Allowed characters: 0-9";
+                    isVolume = true;
+                }
+                else
+                {//ready for storage
+                    lbl_Volume.Text = " ";
+                    isVolume = false;
+                }
+            }
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
         {
 
         }
